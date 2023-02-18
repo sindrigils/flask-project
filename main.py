f288f14cd40a896fe6fdaa1c23c8f98aade6d01e
Sindri Gils <sindrigils02@gmail.com>
2023-02-18 17:22:40 +0000

    added html and css

--- main.py
+++ main.py
@@ -2,9 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        "id": 1,
+        "title": "Data Analyst",
+        "location": "Bangaluru, India",
+        "salary": "Rs. 1,00,00"
+    },
+
+    {
+        "id": 2,
+        "title": "Data Scientist",
+        "location": "Delhi, India",
+        "salary": "Rs. 12,00,00"
+    },
+
+    {
+        "id": 3,
+        "title": "Backend Engineer",
+        "location": "San Francisco, USA",
+        "salary": "$120,000"
+    }
+]
+
 @app.route("/")
 def print_hello():
-    return render_template("home.html")
+    return render_template("home.html", jobs=JOBS)
 
 
 if __name__ == "__main__":
