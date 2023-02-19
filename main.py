92c56d1fbd39d55ee7c6d3dd7566cba2921e83af
Sindri Gils <sindrigils02@gmail.com>
2023-02-19 11:11:39 +0000

    added buttons and dynamic data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -29,6 +29,10 @@ JOBS = [
 def print_hello():
     return render_template("home.html", jobs=JOBS)
 
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=(8080))
